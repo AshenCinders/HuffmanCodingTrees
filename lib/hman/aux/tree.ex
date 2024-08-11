57d9f73ff08dbc2f9106d634f815fce04a7fc4af
AshenCinders <albin@edeby.net>
2024-08-11 14:36:54 +0200

    feat: generic 2-child tree structure

---
+++ lib/hman/aux/tree.ex
@@ -0,0 +1,49 @@
+defmodule Hman.Aux.Tree do
+  @moduledoc """
+    Generic recursive 2-child tree structure.
+    Each node stores 1 piece of data, nil by default.
+    Leaf nodes have its children as nil.
+
+    Use Get for reading / traversing.
+    Use Set for writing to the structure.
+  """
+  defmodule Set do
+    def new_node() do
+      {nil, nil, nil}
+    end
+
+    def new_node(data) do
+      {nil, nil, data}
+    end
+
+    def new_node(left, right, data) do
+      {left, right, data}
+    end
+
+    def left(node, child) do
+      put_elem(node, 0, child)
+    end
+
+    def right(node, child) do
+      put_elem(node, 1, child)
+    end
+
+    def data(node, data) do
+      put_elem(node, 2, data)
+    end
+  end
+
+  defmodule Get do
+    def left(node) do
+      elem(node, 0)
+    end
+
+    def right(node) do
+      elem(node, 1)
+    end
+
+    def data(node) do
+      elem(node, 2)
+    end
+  end
+end
